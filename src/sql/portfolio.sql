525e4d5384d6923fbe0bfda140587deac217d57f
tom <tom@minty>
2014-11-19 12:18:11 +0000

    Handle issue where currency returned by yahoo is not always correct

--- src/sql/portfolio.sql
+++ src/sql/portfolio.sql
@@ -2,17 +2,37 @@ select * from
 (
     select
         y.symbol as symbol, 
-        p.currency,
-        p.position, 
-        p.strike, 
-        y.price * f.fx, 
-        y.change * f.fx, 
-        y.change / y.price, 
-        y.volume, 
-        f.fx,
-        f.prtfFx,
-        (y.price * f.fx - p.strike) * p.position,
-        (y.price * f.fx - p.strike) / p.strike
+        p.currency as currency,
+        p.position as position, 
+        p.strike as strike, 
+        y.price * f.fx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end as price, 
+        y.change * f.fx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end as change, 
+        y.change / y.price as pct_change, 
+        y.volume as volume, 
+        f.fx as fx,
+        f.prtfFx as prtf_fx,
+        (y.price * f.fx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end - p.strike) * p.position as total_change,
+        (y.price * f.fx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end - p.strike) / p.strike as total_pct_change
 
         from 
             yahooQuotesTable as y, 
@@ -26,18 +46,43 @@ select * from
      
     union select
         "TOTAL" as symbol,
-        "GBP",
-        0.0,
-        sum(p.position * p.strike * f.prtffx),
-        sum(p.position * y.price * f.fx * f.prtffx),
-        sum(p.position * y.change * f.fx * f.prtffx),
-        sum(p.position * y.change * f.fx * f.prtffx) / sum(p.position * y.price * f.fx * f.prtffx),
-        0.0,
-        0.0,
-        1,
-        sum((y.price * f.fx - p.strike) * p.position * f.prtffx),
-        sum((y.price * f.fx - p.strike) * p.position * f.prtffx) / sum(p.position * p.strike * f.prtffx)
-        
+        "GBP" as currency,
+        0.0 as position,
+        sum(p.position * p.strike * f.prtffx) as strike,
+        sum(p.position * y.price * f.fx * f.prtffx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end) as price,
+        sum(p.position * y.change * f.fx * f.prtffx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end) as change,
+        sum(p.position * y.change * f.fx * f.prtffx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end) / sum(p.position * y.price * f.fx * f.prtffx) as pct_change,
+        0.0 as volume,
+        1.0 as fx,
+        1 as prtf_fx,
+        sum((y.price * f.fx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end - p.strike) * p.position * f.prtffx) as total_change,
+        sum((y.price * f.fx * case 
+                when abs(y.price * f.fx - p.strike) / p.strike > 0.8
+                        then 100.0
+                when abs(y.price * f.fx - p.strike) / y.price > 0.8
+                        then 0.01
+                else 1.0 end - p.strike) * p.position * f.prtffx) / sum(p.position * p.strike * f.prtffx) as total_pct_change
+
         from 
             yahooQuotesTable as y,
             portfolio as p,
